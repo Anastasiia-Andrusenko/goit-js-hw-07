190745cdd2b763b6818365f0a2d9dd78ee25e6aa
Anastasiia Andrusenko <107683074+Anastasiia-Andrusenko@users.noreply.github.com>
2022-11-10 20:24:07 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -16,10 +16,35 @@ const galleryRef = document.querySelector('.gallery');
 
 const makeGalleryMarkup = galleryItem => {
   return `
-    <img src="${galleryItem.preview}" alt="${galleryItem.description}" width="340">
-   `;
+  <div class="gallery__item">
+    <a href="${galleryItem.original}" class="gallery__link">
+      <img
+        src="${galleryItem.preview}" 
+        alt="${galleryItem.description}" 
+        class="gallery__image"
+        data-source="${galleryItem.original}"
+        >
+    </a>
+  </div>`;
 };
 
 const makeGallery = galleryItems.map(makeGalleryMarkup).join('');
-
 galleryRef.insertAdjacentHTML('afterbegin', makeGallery);
+
+
+
+// Реализация делегирования на div.gallery и получение url большого изображения.
+
+function onClickImage(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains('gallery__image')) {
+    return;
+  }
+  const imageLink = evt.target.dataset.source;
+  console.log(imageLink);
+}
+
+
+
+
+galleryRef.addEventListener('click', onClickImage);
